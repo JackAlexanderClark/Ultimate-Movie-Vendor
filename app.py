d0e1ecdb6a27c814fba3e465709cfcbb77720e75
Jack <jackalexanderclark@protonmail.com>
2023-03-30 15:55:51 +0100

    Fix url path for images

--- app.py
+++ app.py
@@ -124,8 +124,8 @@ def submit_dvd_review(id):
         )
         db.session.add(dvd_review)          # add to database
         db.session.commit()                 # commit
-        return redirect(url_for("submit_dvd_review"))
-        #return redirect('/')
+        return redirect(url_for("/add_dvd_review/<int:id>"))
+        return redirect('/')
 
 # update and send id
 @app.route('/update_dvd/<int:id>', methods=["GET", "POST"])
