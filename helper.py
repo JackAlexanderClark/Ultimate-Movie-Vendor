04972481abc46574b70caf535a1bd34fc099fa9b
Jack <jackalexanderclark@protonmail.com>
2023-04-04 21:33:41 +0100

    Add if no reviews 03/04/2023

--- helper.py
+++ helper.py
@@ -2,7 +2,7 @@ from models import DvdReview
 from flask import render_template
 
 # order and sort functions
-def sort_dvd(Dvd, param, id):
+def sort_dvd(Dvd, param):
     if id is None:
         if param == "newest":
             # descending order
@@ -12,19 +12,6 @@ def sort_dvd(Dvd, param, id):
             dvd_model = Dvd.query.join(DvdReview).order_by(DvdReview.rating.desc()).all()
         elif param == "lowest_rating":
             dvd_model = Dvd.query.join(DvdReview).order_by(DvdReview.rating.asc()).all()
-        else:
-            return render_template("index.html", Dvd=Dvd)
-        return dvd_model
-
-    else:
-        if param == "newest":
-            # descending order
-            dvd_model = Dvd.query.filter_by(id=id).order_by(Dvd.created_date.desc()).all()
-        elif param == "highest_rating":
-            # join tables on the primary key
-            dvd_model = Dvd.query.filter_by(id=id).join(DvdReview).order_by(DvdReview.rating.desc()).all()
-        elif param == "lowest_rating":
-            dvd_model = Dvd.query.filter_by(id=id).join(DvdReview).order_by(DvdReview.rating.asc()).all()
         else:
             return render_template("index.html", Dvd=Dvd)
         return dvd_model
