1b21a6264a9c496193a27e93ed0f818bec83f0d4
Jack <jackalexanderclark@protonmail.com>
2023-04-04 18:36:27 +0100

    add id to sort 03/04/2023

--- helper.py
+++ helper.py
@@ -3,14 +3,29 @@ from flask import render_template
 
 # order and sort functions
 def sort_dvd(Dvd, param, id):
-    if param == "newest":
-        # descending order
-        dvd_model = Dvd.query.filter_by(id=id).order_by(Dvd.created_date.desc()).all()
-    elif param == "highest_rating":
-        # join tables on the primary key
-        dvd_model = Dvd.query.filter_by(id=id).join(DvdReview).order_by(DvdReview.rating.desc()).all()
-    elif param == "lowest_rating":
-        dvd_model = Dvd.query.filter_by(id=id).join(DvdReview).order_by(DvdReview.rating.asc()).all()
+    if id is None:
+        if param == "newest":
+            # descending order
+            dvd_model = Dvd.query.order_by(Dvd.created_date.desc()).all()
+        elif param == "highest_rating":
+            # join tables on the primary key
+            dvd_model = Dvd.query.join(DvdReview).order_by(DvdReview.rating.desc()).all()
+        elif param == "lowest_rating":
+            dvd_model = Dvd.query.join(DvdReview).order_by(DvdReview.rating.asc()).all()
+        else:
+            return render_template("index.html", Dvd=Dvd)
+        return dvd_model
+
     else:
-        return render_template("index.html", Dvd=Dvd)
-    return dvd_model
+        if param == "newest":
+            # descending order
+            dvd_model = Dvd.query.filter_by(id=id).order_by(Dvd.created_date.desc()).all()
+        elif param == "highest_rating":
+            # join tables on the primary key
+            dvd_model = Dvd.query.filter_by(id=id).join(DvdReview).order_by(DvdReview.rating.desc()).all()
+        elif param == "lowest_rating":
+            dvd_model = Dvd.query.filter_by(id=id).join(DvdReview).order_by(DvdReview.rating.asc()).all()
+        else:
+            return render_template("index.html", Dvd=Dvd)
+        return dvd_model
+
