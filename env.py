5776f1ce7bde9f117fbe2ce92c04caa863a0f862
Jack <jackalexanderclark@protonmail.com>
2023-04-12 15:20:02 +0100

    Remove env.py from gitignore 05/04/2023

--- env.py
+++
@@ -1,9 +0,0 @@
-import os
-
-os.environ.setdefault("IP", "0.0.0.0")
-os.environ.setdefault("PORT", "5000")
-os.environ.setdefault("SECRET_KEY", "!8z9lF9L16ib")
-os.environ.setdefault("DEBUG", "True")
-os.environ.setdefault("DEVELOPMENT", "True")
-os.environ.setdefault("DB_URL", "postgresql://hluueilm:Z-wW1b3LHa_tAfqDgRTzgyscgQbpoRMP@manny.db.elephantsql.com/hluueilm")
-os.environ.setdefault("POSTGRES_URL", "postgresql://hluueilm:Z-wW1b3LHa_tAfqDgRTzgyscgQbpoRMP@manny.db.elephantsql.com/hluueilm")
