427a0b9059a1c0f53d882deb84430e8d3101365f
Jack <jackalexanderclark@protonmail.com>
2023-04-14 11:12:12 +0100

    deploy app 05/04/2023

---
+++ env.py
@@ -0,0 +1,9 @@
+import os
+
+os.environ.setdefault("IP", "0.0.0.0")
+os.environ.setdefault("PORT", "5000")
+os.environ.setdefault("SECRET_KEY", "!8z9lF9L16ib")
+os.environ.setdefault("DEBUG", "False")
+os.environ.setdefault("DEVELOPMENT", "False")
+os.environ.setdefault("DATABASE_URL", "postgresql://puahdvpc:81EioKKRFsBHFqhBeD5CxgzfvWZMgDPe@kandula.db.elephantsql.com/puahdvpc")
+os.environ.setdefault("POSTGRES_URL", "postgresql://puahdvpc:81EioKKRFsBHFqhBeD5CxgzfvWZMgDPe@kandula.db.elephantsql.com/puahdvpc")
